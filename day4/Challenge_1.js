0bc945ab332420bf0b751ba644966ae580b1b61a
abdelkhalek harrab <abdelkhalekhe007@gmail.com>
2026-09-10 12:25:26 +0200

    compterCaracteres

---
+++ day4/Challenge_1.js
@@ -0,0 +1,22 @@
+function compterCaracteres(chaine)
+{
+    let j = 0
+    for(let i of chaine)
+    {
+        j++;
+    }
+    console.log(j);
+}
+
+compterCaracteres("Code");
+
+
+
+
+
+// let s = "ahmed" ;
+
+// for(let i of s)
+// {
+//     console.log(i);
+// }
